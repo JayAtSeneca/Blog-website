7ca54fb6408b2a7164fb0737bfa0fd13f6ccc107
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-06-03 10:52:59 -0400

    updated server.js to add name and other info

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Jay Pravinkumar Chaudhari Student ID: 147268205 Date: 2022-06-03
+*
+*  Online (Heroku) URL: https://lit-castle-17301.herokuapp.com/
+*
+*  GitHub Repository URL: https://github.com/JayAtSeneca/web322-app
+*
+********************************************************************************/ 
 var express = require("express");
 var app = express();
 var path = require("path");
